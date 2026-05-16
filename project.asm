a913c5bb84ee6d2cab992c980aeadc2eac42a129
python codes <usman1236666@gmail.com>
2026-05-17 03:12:08 +0500

    linking attendence with student

--- project.asm
+++ project.asm
@@ -18,17 +18,18 @@
      marksin : db 13,10,"Please enter student Marks : $",13,10
 
      ;student out messages
+     norecord : db 13,10,"!! Student table has no record !!$",13,10
      stdname: db 13,10,10,"name of the student is : $";0-4
      stdroll: db 13,10,"Roll Number of the student is : $";5
      stdage: db 13,10,"Age of the student is : $";6-7
      stdfee: db 13,10,"Fee Status of the student is : $";8
      stdmarks: db 13,10,"Marks of the student are : $",13,10;9-10
-     std1:db 0,0,0,0,0,0,0,0,0,0,0
-     std2:db 0,0,0,0,0,0,0,0,0,0,0
-     std3:db 0,0,0,0,0,0,0,0,0,0,0
-     std4:db 0,0,0,0,0,0,0,0,0,0,0
-     std5:db 0,0,0,0,0,0,0,0,0,0,0
-     stdcount : db 0
+     student db 0,0,0,0,0,0,0,0,0,0,0   ;0-10 std1
+        db 0,0,0,0,0,0,0,0,0,0,0    ;11-21 std2
+        db 0,0,0,0,0,0,0,0,0,0,0    ;22-32 std3
+        db 0,0,0,0,0,0,0,0,0,0,0    ;33-43 std4
+        db 0,0,0,0,0,0,0,0,0,0,0    ;44-54 std5
+     stdpointer : db 0
      ;student end
      
      ;attendence
@@ -38,12 +39,13 @@
      space : db " : $"
      slash : db " | $"
      attenout: db 13,10,"attendence record of rollnumber $"
-     atten  db 1,0,0,0,0,0 ;0-5 s1
-            db 2,0,0,0,0,0 ;6-11 s2
-            db 3,0,0,0,0,0 ;12-17 s3
-            db 4,0,0,0,0,0 ;18-23 s4
-            db 5,0,0,0,0,0 ;24-29 s5
-     atpointer: db 0 ;1,2,3,4,5
+     atten  db 0,0,0,0,0,0 ;0-5 s1 0 serial number
+            db 0,0,0,0,0,0 ;6-11 s2 6 serial number
+            db 0,0,0,0,0,0 ;12-17 s3 12 serial number
+            db 0,0,0,0,0,0 ;18-23 s4 18 serial number
+            db 0,0,0,0,0,0 ;24-29 s5 24 serial number
+     atpointercol: dw 0 ;1,2,3,4,5
+     atpointerrow: dw 0
      ;attendence end
 
     start:
@@ -80,17 +82,21 @@
     jmp exit
 
     add_student:
-        mov bx,0 ;counter 0-11  (0-4(name),5(rollnumber),6-7(age),8(fee status),9-10(marks))
+        mov bl,[stdpointer]
+        mov bh,0
+
+        mov si,0 ;counter 0-11  (0-4(name),5(rollnumber),6-7(age),8(fee status),9-10(marks))
         ;Name
+        mov di,[atpointerrow]
         mov ah,09h
         mov dx,namein
         int 21h
         input_name_loop:
             mov ah,01
             int 21h
-            mov [std1+bx],al 
-            add bx,1
-            cmp bx,5
+            mov [student+bx+si],al 
+            add si,1
+            cmp si,5
             jne input_name_loop
         ;Roll Number
         mov ah,09h
@@ -99,9 +105,10 @@
         input_roll_loop:
             mov ah,01
             int 21h
-            mov [std1+bx],al 
-            add bx,1
-            cmp bx,6
+            mov [student+bx+si],al
+            mov [atten+di],al
+            add si,1
+            cmp si,6
             jne input_roll_loop
         ;Age
         mov ah,09h
@@ -110,9 +117,9 @@
         input_age_loop:
             mov ah,01
             int 21h
-            mov [std1+bx],al 
-            add bx,1
-            cmp bx,8
+            mov [student+bx+si],al 
+            add si,1
+            cmp si,8
             jne input_age_loop
         ;Fee status
         mov ah,09h
@@ -121,9 +128,9 @@
         input_fee_loop:
             mov ah,01
             int 21h
-            mov [std1+bx],al 
-            add bx,1
-            cmp bx,9
+            mov [student+bx+si],al 
+            add si,1
+            cmp si,9
             jne input_fee_loop
         ;Marks status
         mov ah,09h
@@ -132,83 +139,119 @@
         input_marks_loop:
             mov ah,01
             int 21h
-            mov [std1+bx],al 
-            add bx,1
-            cmp bx,11
+            mov [student+bx+si],al 
+            add si,1
+            cmp si,11
             jne input_marks_loop
+        add bx,11
+        add di,6
+        mov [atpointerrow],di
+        mov [stdpointer],bx
+        
         
         ret
 
 
     show_student:
-        mov bx,0 ;counter 0-11  (0-4(name),5(rollnumber),6-7(age),8(fee status),9-10(marks))
+        mov bl,0
+        mov bh,0
+        mov di,0
+        cmp byte[stdpointer],0
+        je jump
+        jne outer_show_student
+        jump :
+            jmp skip_dspstudent
+
+        outer_show_student:
+        ;suppose stdpointer is 22 mean we have two students std1 0-10,std2 11-21
+        mov si,0 ;counter 0-11  (0-4(name),5(rollnumber),6-7(age),8(fee status),9-10(marks))
         ;Name
+        
         mov dx,stdname
         mov ah,09h
         int 21h
         output_name_loop:
-            mov dl,[std1+bx]
+            mov dl,[student+si+bx]
             mov ah,02
             int 21h
-            add bx,1
-            cmp bx,5
+            add si,1
+            cmp si,5
             jne output_name_loop
         ;Roll number
         mov dx,stdroll
         mov ah,09h
         int 21h
         output_roll_loop:
-            mov dl,[std1+bx]
+            ;mov dl,[student+si+bx]
+            mov dl,[atten+di]
             mov ah,02
             int 21h
-            add bx,1
-            cmp bx,6
+            add si,1
+            add di,6
+            cmp si,6
             jne output_roll_loop
         ;Age
         mov dx,stdage
         mov ah,09h
         int 21h
         output_age_loop:
-            mov dl,[std1+bx]
+            mov dl,[student+si+bx]
             mov ah,02
             int 21h
-            add bx,1
-            cmp bx,8
+            add si,1
+            cmp si,8
             jne output_age_loop
         ;Fee status
         mov dx,stdfee
         mov ah,09h
         int 21h
         output_fee_loop:
-            mov dl,[std1+bx]
+            mov dl,[student+si+bx]
             mov ah,02
             int 21h
-            add bx,1
-            cmp bx,9
+            add si,1
+            cmp si,9
             jne output_fee_loop
         ;Marks
         mov dx,stdmarks
         mov ah,09h
         int 21h
         output_marks_loop:
-            mov dl,[std1+bx]
+            mov dl,[student+si+bx]
             mov ah,02
             int 21h
-            add bx,1
-            cmp bx,11
+            add si,1
+            cmp si,11
             jne output_marks_loop    
         mov dl,13
+        add bl,11
+        cmp bl,[stdpointer]
+        jl outer_show_student
+        jmp t1
+        skip_dspstudent:
+            mov dx,norecord
+            mov ah,09h
+            int 21h
+        t1:
+
         int 21h
         ret
+
+
+
     take_attendence: 
+       mov di,[atpointerrow]
+       cmp di,0
+       je skip_take_attendence
        mov dx,dsp_takingatten
        mov ah,09h
        int 21h
+       ;di 6,12,26
 
-       mov cl,1
+    ;    mov cl,0
        mov si,0
-       mov bl,[atpointer]
-       mov bh,0
+       mov bx,[atpointercol]
+    ;    mov bh,0
        add bx,1
        label:
        ;// input show block
@@ -217,7 +260,7 @@
               int 21h
 
               mov ch,[atten+si]
-              add ch,30h
+              ;add ch,30h
               mov dl ,ch
               mov ah,02h
               int 21h
@@ -234,22 +277,32 @@
 
               mov [atten+si+bx],al
        ;end iput block
-              add cl,1
+            ;   add cl,6
               add si,6
-              cmp cl,6
-              jne label
-       mov [atpointer],bl
+              cmp si,[atpointercol]
+              jl label
+       mov [atpointercol],bx
+       jmp t2
+       skip_take_attendence:
+            mov dx,norecord
+            mov ah,09h
+            int 21h
+       t2:
+
      ret
      show_attendence:
+       mov di,[atpointerrow]
+       cmp di,0
+       je skip_show_attendence
        mov dx,dsp_atten
        mov ah,09h
        int 21h
      ;[atpointer] have the toatal number of attendence so show
      ;attendence upto [atpointer value]
-       mov cl,1
-       mov bl,[atpointer]
+    ;    mov cl,1
+       mov bx,[atpointercol]
        mov bh,0
-       cmp bl,0
+       ;cmp bl,0
        je endb
        mov si,0
        show_outer:
@@ -259,10 +312,10 @@
               mov ah,09h
               int 21h
 
-              ; mov ch,[atten+si]
-              ; add ch,30h
-              mov dl ,cl
-              add dl,30h
+              mov ch,[atten+si]
+            ;   add ch,30h
+              mov dl ,ch
+            ;   add dl,30h
               mov ah,02h
               int 21h
 
@@ -281,13 +334,19 @@
 
                      add bp,1
                      cmp bp,bx 
-                     jle show_inner
+                     jl show_inner
 
               add cl,1
               add si,6
-              cmp cl,6
-              jne show_outer
+              cmp si,[atpointerrow]
+              jl show_outer
               endb:
+                jmp t3
+                skip_show_attendence:
+                    mov dx,norecord
+                    mov ah,09h
+                    int 21h
+                t3:
               ret
         
     exit:
